5688e9b8dccc97a9b4342b0cc7e8c6e15ef6c0e8
Oluwaseun Adeyo <johnadeyo@hotmail.com>
2023-12-19 16:03:06 +0100

    implemented streamlit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
     notes = st.text_area("Enter product information:")
     if st.button("Generate description"):
         with st.spinner("Generating description..."):
-            response = openai.Completion.create(
+            response = openai.completions.create(
               model="text-davinci-003",
               prompt=f"Write a product description based on the below information.\n\n{notes}\n\nDescription:",
               temperature=0.7,
@@ -18,7 +18,7 @@ def main():
               frequency_penalty=0,
               presence_penalty=0
             )
-        description = response['choices'][0]['text']
+        description = response.choices[0].text
         st.subheader("Generated description:")
         st.write(description)
 
